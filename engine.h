7e10178c0ecc84bcfdf4315f650f49916c227408
gabrielpaesdev <gabriel.paesbarreto@ufrpe.br>
2026-07-14 21:27:07 +0000

    Update engine.h

--- engine.h
+++ engine.h
@@ -5,14 +5,13 @@
 
 #define MAX_NUM 100
 
-/* Funções de lógica do jogo chamadas pela interface (main.c) */
 void start_easy(GtkWidget *w, gpointer data);
 void start_medium(GtkWidget *w, gpointer data);
 void start_hard(GtkWidget *w, gpointer data);
 void on_number_clicked(GtkWidget *widget, gpointer data);
 void restart_game(GtkWidget *w, gpointer data);
 
-/* Função auxiliar para atualizar botões (se necessário expor, mas geralmente interna) */
+
 void update_buttons();
 
 #endif
